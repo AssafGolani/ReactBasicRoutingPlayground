444334de57db7eb483f4d1b4c02df70d44ae59cd
Assaf <1214assaf@gmail.com>
2024-05-22 18:40:45 +0300

    feat: Add navigation to Products page using programmaticaly navigation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,16 @@
+import { useNavigate } from "react-router-dom";
 function HomePage() {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate("/products");
+  }
   return (
     <>
       <h1>My home page</h1>
+      <p>
+        <button onClick={navigateHandler}>NAVIGATE</button>
+      </p>
     </>
   );
 }
