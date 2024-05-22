68d7c21a79d5f3e2a27915f015e443618b49a0bb
Assaf <1214assaf@gmail.com>
2024-05-22 16:19:58 +0300

    refactor: Update routing configuration not using createRoutesFromElements

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import {
   createBrowserRouter,
-  createRoutesFromElements,
-  Route,
+  // createRoutesFromElements,
+  // Route,
   RouterProvider,
 } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 
-const routesDefinition = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/products" element={<ProductsPage />} />
-  </Route>
-);
+// const routesDefinition = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
 
-// const router = createBrowserRouter([
-//   { path: "/", element: <HomePage /> },
-//   {
-//     path: "/products",
-//     element: <ProductsPage />,
-//   },
-// ]);
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  {
+    path: "/products",
+    element: <ProductsPage />,
+  },
+]);
 
-const router = createBrowserRouter(routesDefinition);
+// const router = createBrowserRouter(routesDefinition);
 
 function App() {
   return <RouterProvider router={router} />;
