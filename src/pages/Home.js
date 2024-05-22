bd249829461d67bc213e0e63efac62d642fe0078
Assaf <1214assaf@gmail.com>
2024-05-22 17:40:23 +0300

    feat: Update navigation links to use NavLink instead of Link
    The code changes update the navigation links in the MainNavigation component to use NavLink instead of Link from the react-router-dom library. This change allows for better styling and active link highlighting.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,7 @@
-import { Link } from "react-router-dom";
 function HomePage() {
   return (
     <>
       <h1>My home page</h1>
-      <p> Go To</p> <Link to="/products">the list of products</Link>
     </>
   );
 }
