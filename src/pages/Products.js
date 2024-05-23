913997361a4ef76e32e9db6582951cf84ca62551
Assaf <1214assaf@gmail.com>
2024-05-23 10:27:36 +0300

    feat: Add ProductDetailPage component and update routing configuration
    The code changes add the ProductDetailPage component to handle displaying product details and update the routing configuration in the App.js file to include the new route for "/products/:productId". This enhancement improves the functionality of the application by allowing users to view specific product details.

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,4 +1,13 @@
 function ProductsPage() {
-  return <h2>Prodcuts page</h2>;
+  return (
+    <>
+      <h2>Prodcuts page</h2>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
+    </>
+  );
 }
 export default ProductsPage;
